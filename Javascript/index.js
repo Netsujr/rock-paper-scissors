b4468272cfa05cb1c177372e9d82510ea51694a6
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-17 20:00:30 +0900

    js not choosing correct clicked item

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,5 +1,20 @@
+// dictionary data structure \/
+const handOptions = {
+  "rock": "/assets/Rock.png",
+  "paper": "/assets/Paper.png",
+  "scissors": "/assets/Scissors.png"
+}
+
+
 const pickUserHand = (hand) => {
   // console.log(hand);
   let hands = document.querySelector(".hands");
   hands.style.display = "none"
+
+  let contest = document.querySelector(".contest");
+  contest.style.display = "flex";
+
+  // setting user pick
+  document.getElementById("userPickImage").src = handOptions(hand);
+
 }
